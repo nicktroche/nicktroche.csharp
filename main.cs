7d8f00a8fe2fffd3b2e1fbee311f2415e882d4f1
nicktroche <nicholaswtroche@gmail.com>
2024-06-11 19:19:13 +0000

    mod 1 john

--- main.cs
+++ main.cs
@@ -4,6 +4,6 @@ class Program {
   public static void Main (string[] args) 
   {
     //The next line shows what will be displayed on the console.
-    Console.WriteLine("Lucinda Potter \n6/24/1992 \nwork 000-000-0101 \ncell 000-000-0189");
+    Console.WriteLine("John Smith \n8/15/1978 \nwork 000-111-0101 \ncell 000-111-0189");
     }
 }
