8df33c39d161784ad4e445b72e8cdd042b72deae
nicktroche <nicholaswtroche@gmail.com>
2024-06-11 19:09:37 +0000

    mod 1

--- main.cs
+++ main.cs
@@ -1,7 +1,9 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+  public static void Main (string[] args) 
+  {
+    //The next line shows what will be displayed on the console.
+    Console.WriteLine("Lucinda Potter \n6/24/1992 \nwork 000-000-0101 \ncell 000-000-0189");
+    }
 }
